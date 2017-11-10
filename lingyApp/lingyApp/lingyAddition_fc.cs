c2b6099c06e887ab9d202de592a21032c9d69980
Phudech Thanthip - Together TH <t.phudech@togetherteam.co.th>
2017-11-10 14:56:32 +0700

    test jaaaaaaaaaaaaaaaaa

--- lingyApp/lingyApp/lingyAddition_fc.cs
+++ lingyApp/lingyApp/lingyAddition_fc.cs
@@ -28,7 +28,7 @@ namespace lingyApp
             int b = int.Parse(paramB);
             int sum = a + b;
             string result = sum.ToString();
-            string hallo = "hiiiiii";
+            string hallo = "hiiiiiijaaaaaa";
 
             // Get request body
             dynamic reqDataA = await req.Content.ReadAsAsync<object>();
